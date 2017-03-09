3af24e7026ff2870d66b852b6e7b5639523564e0
mihirvijdeshpande <mihirvijdeshpande@gmail.com>
2017-03-09 15:28:28 +0530

    added files

---
+++ src/main/java/com/cloudbees/mvntest/core/Logger.java
@@ -0,0 +1,14 @@
+package  com.cloudbees.mvntest.core;
+
+/**
+ * Core logger class.
+ *
+ */
+public class Logger 
+{
+    private Object unused;
+    public static void log(String msg)
+    {
+        System.out.println( msg + "hello" );
+    }
+}
